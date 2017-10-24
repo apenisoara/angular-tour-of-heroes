d93b05d33fd151ee2274a2edaceca0c29dab30f1
Adrian Penisoara <ady@Adrians-MacBook-Pro.local>
2017-10-24 21:42:48 +0200

    Reformat main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ath-root',
-  templateUrl: './app.component.html',
+  template: `<h1>{{title}}</h1><h2>{{hero}} details !</h2>`,
+  // templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ath';
+  title = 'Tour of Heroes';
+  hero = 'Windstorm';
 }
